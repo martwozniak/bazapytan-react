da1c53956a640bdcb60ae7d4576684e61b378147
Martin <persky03@gmail.com>
2021-06-24 10:05:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header.js';
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+
+        <p>
+          Tutaj będzie treść strony
+        </p>
+     
+    </div>
+  );
+}
+
+export default App;
